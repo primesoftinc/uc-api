e0206e35697d41430f3ae98c6e9a1bee0a9aeb81
anurag <dev.anurag.alla@gmail.com>
2019-08-08 11:13:16 +0530

    config

---
+++ src/main/java/com/prime/uc/repo/BaseRepo.java
@@ -0,0 +1,11 @@
+package com.prime.uc.repo;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepo <T, ID extends Serializable> extends JpaRepository<T, ID>{
+
+}
