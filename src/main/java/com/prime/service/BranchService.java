55de414d29e8192640c3078a41c79e150ce3b25e
jayalakshmi <you@example.com>
2019-09-03 16:18:19 +0530

    Added update method for edit

--- src/main/java/com/prime/service/BranchService.java
+++ src/main/java/com/prime/service/BranchService.java
@@ -3,7 +3,10 @@ package com.prime.service;
 import java.util.List;
 import java.util.UUID;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 
 import com.prime.uc.model.Branch;
@@ -34,6 +37,19 @@ public class BranchService {
 	@GraphQLQuery(name = "getBranchId")
     public BranchUser getBranchId(@GraphQLArgument(name="id")UUID id) {
         return branchRepo.retriveBranchByUserId(id);
+    }
+	
+	
+	@GraphQLMutation(name = "updateBranch")
+    public int updateBranch(@GraphQLArgument(name = "branch") String branchName ,
+    		@GraphQLArgument(name = "code") String code,
+    		@GraphQLArgument(name = "mobile") String mobile,
+    		@GraphQLArgument(name = "landPhone") String landPhone,
+    		@GraphQLArgument(name = "email") String email,
+    		@GraphQLArgument(name = "address") String address,
+    		@GraphQLArgument(name = "contact") String contact,
+    		@GraphQLArgument(name = "id") UUID id) {
+        return branchRepo.update(branchName,code,mobile,landPhone,email,address,contact,id);
     }
 	
 
--- src/main/java/com/prime/uc/repo/BranchRepo.java
+++ src/main/java/com/prime/uc/repo/BranchRepo.java
@@ -3,7 +3,10 @@ package com.prime.uc.repo;
 import java.util.List;
 import java.util.UUID;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -24,5 +27,12 @@ public interface BranchRepo extends JpaRepository<Branch, UUID> {
 	
 	@Query("select bu from BranchUser bu join fetch bu.branch where bu.user.id = ?1")
 	 BranchUser retriveBranchByUserId(UUID id);
+
+	@Transactional
+	@Modifying
+	@Query("update Branch b set b.branchName = ?1, b.code=?2,b.mobile =?3,"
+			+ " b.landPhone = ?4 , b.email = ?5 , b.address = ?6 , b.contact = ?7 where b.id =?8")
+	int update(String branchName, String code, String mobile, String landPhone, String email, String address,
+			String contact, UUID id);
 
 }
