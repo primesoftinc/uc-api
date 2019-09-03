782323fb0f744f139688813ed1106997beea0f7d
mounika <mounika.p@alphaclinicalsystems.com>
2019-09-03 11:31:42 +0530

    added select privilege query

--- src/main/java/com/prime/uc/repo/PrivilegeRepo.java
+++ src/main/java/com/prime/uc/repo/PrivilegeRepo.java
@@ -1,11 +1,21 @@
 package com.prime.uc.repo;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.prime.uc.model.Privilege;
 
+@Repository
 public interface PrivilegeRepo extends JpaRepository<Privilege, UUID>{
+
+//	@Query("select privilege from Privilege privilege")
+//	List<Privilege> retrive();
+	
+	@Query("select privilege from Privilege privilege")
+	List<Privilege> findAll();
 
 }
