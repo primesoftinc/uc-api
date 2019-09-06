44fbce23b3598a0aa2995a91227c404783251daf
mounika <mounika.p@alphaclinicalsystems.com>
2019-09-06 15:17:06 +0530

    added query to retieve  DoctorSpecialization

--- src/main/java/com/prime/service/DoctorSpecializationService.java
+++ src/main/java/com/prime/service/DoctorSpecializationService.java
@@ -1,5 +1,6 @@
 package com.prime.service;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +9,7 @@ import com.prime.uc.repo.DoctorSpecializationRepo;
 
 import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 
 @Service
@@ -20,5 +22,10 @@ public class DoctorSpecializationService {
 	@GraphQLMutation(name = "saveDoctorSpecialization")
     public DoctorSpecialization saveDoctorSpecialization(@GraphQLArgument(name = "user") DoctorSpecialization doctorSpecialization) {
         return doctorSpecializationRepo.save(doctorSpecialization);
+    }
+	
+	@GraphQLQuery(name = "getDoctorSpecialization")
+    public List<DoctorSpecialization> getDoctorSpecialization(){
+    	return doctorSpecializationRepo.getSpecializations();
     }
 }
--- src/main/java/com/prime/uc/repo/DoctorSpecializationRepo.java
+++ src/main/java/com/prime/uc/repo/DoctorSpecializationRepo.java
@@ -1,8 +1,10 @@
 package com.prime.uc.repo;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.prime.uc.model.DoctorSpecialization;
@@ -10,5 +12,8 @@ import com.prime.uc.model.DoctorSpecialization;
 
 @Repository
 public interface DoctorSpecializationRepo extends JpaRepository<DoctorSpecialization, UUID>{
+
+	@Query("select ds from DoctorSpecialization ds")
+	List<DoctorSpecialization> getSpecializations();
 
 }
