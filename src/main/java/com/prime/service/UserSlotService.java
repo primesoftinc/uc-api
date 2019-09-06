ff02e353c668df40ab432ff6e46d68c95ddf9377
jayalakshmi <you@example.com>
2019-09-06 15:29:05 +0530

    Added a method to view Appoinments in doctorList

--- src/main/java/com/prime/service/UserSlotService.java
+++ src/main/java/com/prime/service/UserSlotService.java
@@ -1,5 +1,8 @@
 package com.prime.service;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +11,7 @@ import com.prime.uc.repo.UserSlotRepo;
 
 import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 
 @Service
@@ -15,10 +19,18 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 public class UserSlotService {
 	@Autowired
 	private UserSlotRepo userSlotRepo;
+	
 	@GraphQLMutation(name = "saveUserSlot")
     public UserSlot saveUserSlot(@GraphQLArgument(name = "userSlot") UserSlot userSlot) {
         return userSlotRepo.save(userSlot);
     }
+	
+	
+	@GraphQLQuery(name = "getAppointments")
+    public List<UserSlot> getAppointments(@GraphQLArgument(name = "doctorId") UUID doctorId) {
+        return userSlotRepo.getAppointmentsOrderByDate(doctorId);
+    }
+	
 	
 
 }
--- src/main/java/com/prime/uc/repo/UserSlotRepo.java
+++ src/main/java/com/prime/uc/repo/UserSlotRepo.java
@@ -1,14 +1,21 @@
 package com.prime.uc.repo;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.prime.uc.model.UserSlot;
 
 @Repository
 public interface UserSlotRepo  extends JpaRepository<UserSlot, UUID> {
+	
+	@Query("select us from UserSlot us join fetch us.doctorSlot ds join fetch ds.doctor d where d.id =?1")
+	List<UserSlot> getAppointmentsOrderByDate(UUID doctorId);
+
+	
 	
 
 }
