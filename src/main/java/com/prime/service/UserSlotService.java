1cc80e059141a2c7c12dd467bd2c7a5eee852d13
mounika <mounika.p@alphaclinicalsystems.com>
2019-09-06 15:26:09 +0530

    added saveUser

--- src/main/java/com/prime/service/UserSlotService.java
+++ src/main/java/com/prime/service/UserSlotService.java
@@ -1,13 +1,17 @@
 package com.prime.service;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.prime.uc.model.BranchUser;
 import com.prime.uc.model.UserSlot;
 import com.prime.uc.repo.UserSlotRepo;
 
 import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 
 @Service
@@ -18,6 +22,11 @@ public class UserSlotService {
 	@GraphQLMutation(name = "saveUserSlot")
     public UserSlot saveUserSlot(@GraphQLArgument(name = "userSlot") UserSlot userSlot) {
         return userSlotRepo.save(userSlot);
+    }
+	
+	@GraphQLQuery(name = "getUserSlotId")
+    public UserSlot getBranchId(@GraphQLArgument(name="id")UUID id) {
+        return userSlotRepo.retriveUserSlotByUserBranchId(id);
     }
 	
 
--- src/main/java/com/prime/uc/repo/UserSlotRepo.java
+++ src/main/java/com/prime/uc/repo/UserSlotRepo.java
@@ -3,12 +3,17 @@ package com.prime.uc.repo;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.prime.uc.model.UserSlot;
 
 @Repository
 public interface UserSlotRepo  extends JpaRepository<UserSlot, UUID> {
+
+	
+	@Query("select us from UserSlot us join fetch us.branch where us.branch.id = ?1")
+	UserSlot retriveUserSlotByUserBranchId(UUID id);
 	
 
 }
