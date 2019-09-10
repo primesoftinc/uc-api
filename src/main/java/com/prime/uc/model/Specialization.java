47dd4ea43ccc4e4bcb2f83eabf6041116708b404
mounika <mounika.p@alphaclinicalsystems.com>
2019-09-10 15:07:58 +0530

    added Specialization entity,service and repo

---
+++ src/main/java/com/prime/uc/model/Specialization.java
@@ -0,0 +1,31 @@
+package com.prime.uc.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "specialization")
+@Getter @Setter
+@NoArgsConstructor
+@ToString
+public class Specialization extends BaseEntity{
+	@Column(name="specialization_name")
+    private String specializtionName;
+
+	@JoinColumn(name = "branch_id")
+	@ManyToOne(fetch=FetchType.LAZY)
+    private Branch branch;
+
+}
