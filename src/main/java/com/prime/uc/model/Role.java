cd5b6049f6c5ac8e8a7eab2750e25bc134252c70
mounika <mounika.p@alphaclinicalsystems.com>
2019-09-10 15:05:25 +0530

    removed CascadeType to branch_id

--- src/main/java/com/prime/uc/model/Role.java
+++ src/main/java/com/prime/uc/model/Role.java
@@ -2,6 +2,7 @@ package com.prime.uc.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -26,7 +27,7 @@ public class Role extends BaseEntity{
     private String roleName;
 	
 	@JoinColumn(name = "branch_id")
-	@ManyToOne(fetch=FetchType.LAZY)
+	@ManyToOne(fetch=FetchType.LAZY,cascade=CascadeType.ALL)
     private Branch branch;
 	
 	@OneToMany(mappedBy="role")
