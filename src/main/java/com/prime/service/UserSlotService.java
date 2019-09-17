1c110e43db5dfbe2131c7f260c09b6486349a5ad
jayalakshmi <you@example.com>
2019-09-17 11:39:47 +0530

    Update status and Notes Queries

--- src/main/java/com/prime/service/UserSlotService.java
+++ src/main/java/com/prime/service/UserSlotService.java
@@ -1,13 +1,14 @@
 package com.prime.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.prime.uc.model.BranchUser;
 import com.prime.uc.model.UserSlot;
+import com.prime.uc.repo.UserRepo;
 import com.prime.uc.repo.UserSlotRepo;
 
 import io.leangen.graphql.annotations.GraphQLArgument;
@@ -20,15 +21,45 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 public class UserSlotService {
 	@Autowired
 	private UserSlotRepo userSlotRepo;
+	@Autowired
+	private UserRepo userRepo;
+	
 	
 	@GraphQLMutation(name = "saveUserSlot")
     public UserSlot saveUserSlot(@GraphQLArgument(name = "userSlot") UserSlot userSlot) {
         return userSlotRepo.save(userSlot);
     }
+	@GraphQLMutation(name = "updateUserSlot")
+    public UserSlot updateUserSlot(@GraphQLArgument(name = "userSlot") UserSlot userSlot) {
+        userRepo.save(userSlot.getUser());
+         userSlot = userSlotRepo.save(userSlot);
+        return userSlot;
+    }
+	
+	@GraphQLMutation(name = "updateUserSlotNotes")
+	public int updateUserSlotNotes(@GraphQLArgument(name = "notes") String notes ,
+			@GraphQLArgument(name = "id") UUID id) {
+	        return userSlotRepo.updateUserSlotNotes(notes,id);
+	}
+	@GraphQLMutation(name = "updateUserSlotStatus")
+	public int updateUserSlotStatus(@GraphQLArgument(name = "status") String status ,
+			@GraphQLArgument(name = "id") UUID id) {
+		if(status.equals("cancelled")) {
+			userSlotRepo.updateUserSlotCancell(id,true);
+		}else {
+			userSlotRepo.updateUserSlotCancell(id,false);
+		}
+	        return userSlotRepo.updateUserSlotStatus(status,id);
+	}
 	
 	@GraphQLQuery(name = "getUserSlotId")
     public UserSlot getBranchId(@GraphQLArgument(name="id")UUID id) {
         return userSlotRepo.retriveUserSlotByUserBranchId(id);
+    }
+	
+	@GraphQLQuery(name = "getUserSlotById")
+    public Optional<UserSlot> getUserSlotById(@GraphQLArgument(name="id")UUID id) {
+        return userSlotRepo.findById(id);
     }
 	
 	
--- src/main/java/com/prime/uc/repo/UserSlotRepo.java
+++ src/main/java/com/prime/uc/repo/UserSlotRepo.java
@@ -3,7 +3,10 @@ package com.prime.uc.repo;
 import java.util.List;
 import java.util.UUID;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -23,7 +26,23 @@ public interface UserSlotRepo  extends JpaRepository<UserSlot, UUID> {
 	@Query("select us from UserSlot us join fetch us.doctorSlot join fetch us.user where  us.branch.id =?1 and us.date = ?2")
 	List<UserSlot> retriveAppointmentsByBranchId(UUID branchId,String date);
 
-	@Query("select us from UserSlot us join fetch us.doctorSlot join fetch us.user where us.date between ?1 and ?2")
-	List<UserSlot> getAppointmentsBetweenDate(String fromDate, String toDate);
+	@Query("select us from UserSlot us join fetch us.doctorSlot join fetch us.user where us.branch.id = ?1 and us.date between ?2 and ?3 ")
+	List<UserSlot> getAppointmentsBetweenDate(UUID branchId ,String fromDate, String toDate);
+	
+	@Transactional
+	@Modifying
+	@Query("update UserSlot us set us.notes = ?1 where us.id =?2")
+	int  updateUserSlotNotes(String notes, UUID id);
+	
+	@Transactional
+	@Modifying
+	@Query(value = "update UserSlot us set us.cancelled = ?2  where us.id =?1")
+	int  updateUserSlotCancell( UUID id,Boolean s);
+
+
+	@Transactional
+	@Modifying
+	@Query("update UserSlot us set us.status = ?1 where us.id =?2")
+	int updateUserSlotStatus(String status, UUID id);
 
 }
