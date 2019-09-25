dd0b20ce19d9887814263e7a367ad1ee2ad5ca6d
mounika <mounika.p@alphaclinicalsystems.com>
2019-09-25 14:07:32 +0530

    added query getBranchUserById

--- src/main/java/com/prime/service/BranchUserService.java
+++ src/main/java/com/prime/service/BranchUserService.java
@@ -1,5 +1,6 @@
 package com.prime.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collector;
@@ -7,7 +8,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import com.prime.uc.model.Branch;
 import com.prime.uc.model.BranchUser;
 import com.prime.uc.model.Doctor;
 import com.prime.uc.model.DoctorSpecialization;
@@ -21,6 +24,7 @@ import com.prime.uc.repo.UserRoleRepo;
 
 import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 
 @Service
@@ -43,13 +47,24 @@ public class BranchUserService {
 	@Autowired
 	private DoctorSpecializationRepo doctorSpecializationRepo;
 	
-////	@GraphQLMutation(name = "saveBranchUser")
-//    public BranchUser saveBranchUser(@GraphQLArgument(name = "branchUser") BranchUser branchUser) {
-//        return branchUserRepo.save(branchUser);
-//    }
+	@GraphQLMutation(name = "saveBranchUserDuplicate")
+    public BranchUser saveBranchUser(@GraphQLArgument(name = "branchUser") BranchUser branchUser) {
+       return branchUserRepo.save(branchUser);
+    }
+	
+	
+	 @GraphQLMutation(name = "deleteBranchUserById")
+	    public String deleteBranchUserById(@GraphQLArgument(name = "id") UUID id) {
+	    	branchUserRepo.deleteById(id);
+	         return "delete sucessful";
+	    }
 	
 	@GraphQLMutation(name = "saveBranchUser")
     public BranchUser saveBranchUserWithUser(@GraphQLArgument(name = "branchUser") BranchUser branchUser) {
+		boolean isDoctor;
+		isDoctor = branchUser.getUser().getIsDoctor();
+		List<DoctorSpecialization> doctorSpecialization = new ArrayList<DoctorSpecialization>();
+		//List<DoctorSpecialization> doctorSpecialization =  branchUser.getUser().getDoctors().get(0).getDoctorSpecializations();
 		User user= userRepo.save(branchUser.getUser());
 		user.setUserRoles(branchUser.getUser().getUserRoles());
 		branchUser.setUser(user);
@@ -59,22 +74,45 @@ public class BranchUserService {
 		}).collect(Collectors.toList());
 		userRoleRepo.saveAll(userRoles);
 		UUID uid =user.getId();
-		if(user.getIsDoctor()) {
-			Doctor doctor = new Doctor();
-			doctor.setUser(user);
-			doctor.setBranch(branchUser.getBranch());
-			doctor.setDoctorName(user.getName());
+		if(isDoctor) {
+			doctorSpecialization = branchUser.getUser().getDoctors().get(0).getDoctorSpecializations();
+			Doctor doctor = null;
+			if(branchUser.getUser().getDoctors().stream().findFirst().isPresent()) {
+				doctor = branchUser.getUser().getDoctors().stream().findFirst().get();
+				if(StringUtils.isEmpty(doctor.getId())) {
+					doctor.setUser(user);
+					doctor.setBranch(branchUser.getBranch());
+					doctor.setDoctorName(user.getName());
+				}
+			}
 			doctorRepo.save(doctor);
-			List<DoctorSpecialization> doctorSpecialization = user.getDoctor().get(0).getDoctorSpecializations();
-			doctorSpecialization.stream().map(ds -> {
+//			List<DoctorSpecialization> doctorSpecialization = user.getDoctors().get(0).getDoctorSpecializations();
+			for (DoctorSpecialization ds : doctorSpecialization) {
 				ds.setDoctor(doctor);
-//				ds.setSpecialization(ds.getSpecialization());
-				return ds;
-			}).collect(Collectors.toList());
+			}
+			
+//			doctorSpecialization.stream().map(ds -> {
+//				ds.setDoctor(doctor);
+////				ds.setSpecialization(ds.getSpecialization());
+//				return ds;
+//			}).collect(Collectors.toList());
 			 
 			doctorSpecializationRepo.saveAll(doctorSpecialization);
 		}
 //		UUID urid = ur.getId();
         return branchUserRepo.save(branchUser);
     }
+	
+	@GraphQLQuery(name = "getBranchUserById")
+    public BranchUser getBranchUserById(@GraphQLArgument(name = "id") UUID id) {
+       BranchUser branchUser = branchUserRepo.getBranchUserById(id);
+       if(branchUser.getUser().getDoctors().get(0).getDoctorSpecializations().size()!=0) {
+    	   branchUser.getUser().setIsDoctor(true);
+       }
+	return branchUser;
+       
+    }
+	
+	
+	
 }
