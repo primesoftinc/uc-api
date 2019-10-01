656df50cfc2289ce97dd1cbfea139a40f743dc69
mounika <mounika.p@alphaclinicalsystems.com>
2019-10-01 16:47:35 +0530

    added query for unavailability list

--- src/main/java/com/prime/service/DoctorUnavailabilityService.java
+++ src/main/java/com/prime/service/DoctorUnavailabilityService.java
@@ -1,6 +1,9 @@
 package com.prime.service;
 
 import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.OrderBy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +13,7 @@ import com.prime.uc.repo.DoctorUnavailabilityRepo;
 
 import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 
 @Service
@@ -23,5 +27,11 @@ public class DoctorUnavailabilityService {
     public List<DoctorUnavailability> saveDoctorUnavailability(@GraphQLArgument(name = "doctorUnavailability") List<DoctorUnavailability> doctorUnavailability) 
 	{
         return doctorUnavailabilityRepo.saveAll(doctorUnavailability);
+    }
+	
+
+	@GraphQLQuery(name = "getDoctorUnavailabilityByBranch")
+    public List<DoctorUnavailability> getDoctorUnavailabilityByBranch(@GraphQLArgument(name = "branchId") UUID branchId ,@GraphQLArgument(name = "date") String date) {
+        return doctorUnavailabilityRepo.retriveDoctorUnavailabilityByBranchId(branchId,date);
     }
 }
--- src/main/java/com/prime/uc/repo/DoctorUnavailabilityRepo.java
+++ src/main/java/com/prime/uc/repo/DoctorUnavailabilityRepo.java
@@ -1,13 +1,19 @@
 package com.prime.uc.repo;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.prime.uc.model.DoctorUnavailability;
 
 @Repository
 public interface DoctorUnavailabilityRepo  extends JpaRepository<DoctorUnavailability, UUID> {
+
+	
+	@Query("select du from DoctorUnavailability du join fetch du.doctorSlot ds  where  du.branch.id =?1 and du.date = ?2")
+	List<DoctorUnavailability> retriveDoctorUnavailabilityByBranchId(UUID branchId, String date);
 
 }
